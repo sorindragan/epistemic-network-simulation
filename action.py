63d7bea215be1a42290c853d32b25f7b9a86d2b3
sorindragan <sorin.dragan27@gmail.com>
2021-01-21 20:52:43 +0100

    change implementation to gala and boyal paper; work in progress

--- action.py
+++ action.py
@@ -1,4 +1,7 @@
 import numpy.random as rand
+from scipy.stats import bernoulli
+
+from state import State
 
 class Action:
     def __init__(self) -> None:
@@ -9,8 +12,12 @@ class ActionA(Action):
         super().__init__()
         self.name = "Action A"
 
-    def do(self):
-        return 1 if rand.uniform(0, 1) > 0.5 else 0
+    def do(self, state):
+        if type(state) != type(State(0)):
+            print("The given state was not valid")
+        
+        return bernoulli.rvs(p=0.5, size=1)[0]
+
 
 class ActionB(Action):
     def __init__(self, epsilon) -> None:
@@ -18,5 +25,15 @@ class ActionB(Action):
         self.name = "Action B"
         self.epsilon = epsilon
 
-    def do(self):
-        return 1 if rand.uniform(0, 1) + self.epsilon > 0.5 else 0
+    def do(self, state) -> int:
+        if type(state) != type(State(0)):
+            print("The given state was not valid")
+        
+        if state.id == 1:
+            return bernoulli.rvs(p=(0.5 + self.epsilon), size=1)[0]
+        
+        if state.id == 2:
+            return bernoulli.rvs(p=(0.5 - self.epsilon), size=1)[0]
+        
+        print("It should not reach this point")
+        return 1 if rand.uniform(0, 1) > 0.5 else 0
---
+++ state.py
@@ -0,0 +1,7 @@
+class State:
+    def __init__(self, id) -> None:
+        self.id = id
+    
+    def __repr__(self) -> str:
+        return f"State {self.id}"
+
