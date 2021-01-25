e9d1c225b0c342b4fc1bb9a544aee95b6328d416
sorindragan <sorin.dragan27@gmail.com>
2021-01-25 21:41:32 +0100

    added policymaker and journalist node types; implemented belief convergence graph for sanity check example - look good

--- node.py
+++ node.py
@@ -6,13 +6,16 @@ from action import ActionA, ActionB
 
 VERBOSE = False
 
+
 class AgentNode:
     def __init__(self, id):
         self.id = id
 
+
 class ScientistNode(AgentNode):
     def __init__(self, id):
         super().__init__(id)
+        self.type = "Scientist"
         self.belief = rand.uniform(0, 1)
         self.action_a = ActionA()
         self.action_b = ActionB(0.05)
@@ -21,7 +24,7 @@ class ScientistNode(AgentNode):
         self.neighbours = []
     
     def __repr__(self) -> str:
-        return f"<ID: {self.id} \nBelief: {self.belief} \nNeighbours: {[n.id for n in self.neighbours]}>\n"
+        return f"<Type: {self.type} \nID: {self.id}\nBelief: {self.belief} \nNeighbours: {[n.id for n in self.neighbours]}>\n"
     
     def set_belief(self, belief):
         self.belief = belief
@@ -71,4 +74,122 @@ class ScientistNode(AgentNode):
         if VERBOSE:
             print(f"{self.id} : {self.belief}")
         
+
+class PolicymakerNode(AgentNode):
+    def __init__(self, id):
+        super().__init__(id)
+        self.type = "Policymaker"
+        self.belief = rand.uniform(0, 0.5)
+        self.action_a = ActionA()
+        self.action_b = ActionB(0.05)
+        self.neighbours = []
+
+    def __repr__(self) -> str:
+        return f"<Type: {self.type} \nID: {self.id}\nBelief: {self.belief} \nNeighbours: {[n.id for n in self.neighbours]}>\n"
+
+    def set_belief(self, belief):
+        self.belief = belief
+
+    def add_neighbour(self, neigbour) -> None:
+        if neigbour not in self.neighbours:
+            self.neighbours.append(neigbour)
+
+    def remove_neighbour(self, neighbour) -> None:
+        if neighbour in self.neighbours:
+            self.neighbours.remove(neighbour)
+
+    def define_neighbours(self, neighbours_list) -> None:
+        self.neighbours = neighbours_list
+
+    # Policymakers just update the belief; they do not act
+    def update_belief(self):
+        prior = self.belief
+        nomerator = np.prod([n.pmf for n in self.neighbours]) * prior
+        denominator = sum([np.prod([n.pick_action().do(state)[1]
+                                    for n in self.neighbours]) * (prior if state.id == 1 else (1 - prior))
+                           for state in [State(1), State(2)]])
+        self.belief = nomerator / denominator
+
+        if VERBOSE:
+            print(f"{self.id} : {self.belief}")
+
+
+class JournalistNode(AgentNode):
+    def __init__(self, id):
+        super().__init__(id)
+        self.type = "Journalist"
+        self.belief = rand.uniform(0, 1)
+        self.action_a = ActionA()
+        self.action_b = ActionB(0.05)
+        self.outcome = None
+        self.pmf = None
+        self.neighbours = []
     
+    def __repr__(self) -> str:
+        return f"<Type: {self.type} \nID: {self.id}\nBelief: {self.belief} \nNeighbours: {[n.id for n in self.neighbours]}>\n"
+
+    def set_belief(self, belief):
+        self.belief = belief
+
+    def add_neighbour(self, neigbour) -> None:
+        if neigbour not in self.neighbours:
+            self.neighbours.append(neigbour)
+    
+    def remove_neighbour(self, neighbour) -> None:
+        if neighbour in self.neighbours:
+            self.neighbours.remove(neighbour)
+    
+    def define_neighbours(self, neighbours_list) -> None:
+        self.neighbours = neighbours_list
+
+    def pick_action(self):
+        if self.belief < 0.5:
+            return self.action_a
+        
+        if self.belief >= 0.5:
+            return self.action_b
+        
+        print("This point should not be reached")
+
+    def act(self) -> None:
+        payoff = None
+        p = None
+        if self.belief < 0.5:
+            payoff, p = self.action_a.do(State(1))
+        
+        if self.belief >= 0.5:
+            payoff, p = self.action_b.do(State(1))
+        
+        if payoff is None:
+            print("This point should not have been reached - None Payoff")
+
+        self.outcome, self.pmf = payoff, p
+    
+    # Journalists update their beliefs in a special random way
+    def update_belief(self):
+        neighbour_beliefs = []
+        neighbour_beliefs_le = [n.belief for n in self.neighbours
+                                if n.belief < 0.5
+                                ]
+        neighbour_beliefs_ge = [n.belief for n in self.neighbours
+                                if n.belief >= 0.5
+                                ]
+
+        if len(neighbour_beliefs_le) > 0 and len(neighbour_beliefs_ge) > 0:
+            if rand.uniform(0, 1) < 0.5:
+                neighbour_beliefs = neighbour_beliefs_le
+            else:
+                neighbour_beliefs = neighbour_beliefs_ge
+        elif len(neighbour_beliefs_le) > 0:
+            neighbour_beliefs = neighbour_beliefs_le
+        elif len(neighbour_beliefs_ge) > 0:
+            neighbour_beliefs = neighbour_beliefs_ge
+     
+        if len(neighbour_beliefs) == 0:
+            print("Theoretically, this point shoudl not be reached")
+            neighbour_beliefs = [rand.uniform(0, 1)]
+        
+        self.belief = sum(neighbour_beliefs) / len(neighbour_beliefs)
+ 
+        if VERBOSE:
+            print(f"{self.id} : {self.belief}")
--- simulation.py
+++ simulation.py
@@ -1,4 +1,7 @@
-from node import ScientistNode
+import numpy as np
+import matplotlib.pyplot as plt
+
+from node import JournalistNode, PolicymakerNode, ScientistNode
 from network import generate_ring_lattice, generate_random_network, generate_watts_strogatz_network, generate_barabasi_albert_network
 
 STEPS = 25
@@ -7,6 +10,10 @@ def sanity_check():
     s1 = ScientistNode(1)
     s2 = ScientistNode(2)
     s3 = ScientistNode(3)
+    s4 = ScientistNode(4)
+
+    p1 = PolicymakerNode(5)
+    j1 = JournalistNode(6)
 
     # Belief stays the same after 25 iterations
     # s1.set_belief(0.49360896462333026)
@@ -18,25 +25,67 @@ def sanity_check():
     # s2.set_belief(0.0002)
     # s3.set_belief(0.5)
 
-    s1.define_neighbours([s2, s3])
+    # Interesting test -> The policy maker still converges
+    s1.set_belief(0.55)
+    s2.set_belief(0.6)
+    s3.set_belief(0.1)
+    s4.set_belief(0.1)
+
+    s1.define_neighbours([s4, s2])
     s2.define_neighbours([s1, s3])
-    s3.define_neighbours([s1, s2])
+    s3.define_neighbours([s2, s4])
+    s4.define_neighbours([s3, s1])
+    p1.define_neighbours([s3, j1])
+    j1.define_neighbours([s3, s4])
+
+
 
     print(s1)
     print(s2)
     print(s3)
+    print(s4)
+    print(p1)
+    print(j1)
 
-    for _ in range(25):
-        for s in [s1, s2, s3]:
+    average_belief_all = [np.mean([s.belief for s in [s1, s2, s3, s4, p1, j1]])]
+    average_belief_scientists = [np.mean([s.belief for s in [s1, s2, s3, s4]])]
+    average_belief_policymakers = [p1.belief]
+    average_belief_journalist = [j1.belief]
+    timesteps = [0]
+
+    for t_ in range(25):
+        for s in [s1, s2, s3, s4, j1]:
+            # print(s)
             s.act()
 
-        for s in [s1, s2, s3]:
+        for s in [s1, s2, s3, s4, j1, p1]:
+            # print(s)
             s.update_belief()
-
+        
+        average_belief_all.append(
+            np.mean([s.belief for s in [s1, s2, s3, s4, p1, j1]]))
+        average_belief_scientists.append(
+            np.mean([s.belief for s in [s1, s2, s3, s4]]))
+        average_belief_policymakers.append(p1.belief)
+        average_belief_journalist.append(j1.belief)
+        timesteps.append(t_+1)
 
     print(s1)
     print(s2)
     print(s3)
+    print(s4)
+    print(p1)
+    print(j1)
+
+    fig, ax = plt.subplots(1, figsize=(8, 6))
+    fig.suptitle('Convergence of average belief', fontsize=15)
+    ax.plot(timesteps, average_belief_all, color="black", label="Aggregated Avg Belief")
+    ax.plot(timesteps, average_belief_scientists, color="blue", label="Scientists Avg Belief")
+    ax.plot(timesteps, average_belief_policymakers, color="green", label="Policymakers Avg Belief")
+    ax.plot(timesteps, average_belief_journalist, color="red", label="Journalist Avg Belief")
+    plt.legend(loc="upper left", title="Lines Legend", frameon=False)
+    plt.show()
+
 
 def run_simulation(nodes):
     print(nodes)
@@ -50,9 +99,9 @@ def run_simulation(nodes):
     
 
 def main():
-    # sanity_check()
-    nodes = generate_ring_lattice(N=20)
-    run_simulation(nodes)
+    sanity_check()
+    # nodes = generate_ring_lattice(N=20)
+    # run_simulation(nodes)
 
 
     
